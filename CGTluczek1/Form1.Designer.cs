822a28a7da4cd717d5a316ec4df147f3f75cddc3
tluczekk <tluczekk@o2.pl>
2020-03-06 00:20:30 +0100

    First commit, without bitlocks and layout

---
+++ CGTluczek1/Form1.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace CGTluczek1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.buttonGray = new System.Windows.Forms.Button();
+            this.buttonRevert = new System.Windows.Forms.Button();
+            this.buttonInverse = new System.Windows.Forms.Button();
+            this.buttonBright = new System.Windows.Forms.Button();
+            this.buttonBrght2 = new System.Windows.Forms.Button();
+            this.buttonGamma = new System.Windows.Forms.Button();
+            this.buttonGammaDn = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(586, 395);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(671, 302);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Upload";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // buttonGray
+            // 
+            this.buttonGray.Location = new System.Drawing.Point(671, 29);
+            this.buttonGray.Name = "buttonGray";
+            this.buttonGray.Size = new System.Drawing.Size(75, 23);
+            this.buttonGray.TabIndex = 2;
+            this.buttonGray.Text = "Gray";
+            this.buttonGray.UseVisualStyleBackColor = true;
+            this.buttonGray.Click += new System.EventHandler(this.ButtonGray_Click);
+            // 
+            // buttonRevert
+            // 
+            this.buttonRevert.Location = new System.Drawing.Point(671, 242);
+            this.buttonRevert.Name = "buttonRevert";
+            this.buttonRevert.Size = new System.Drawing.Size(75, 23);
+            this.buttonRevert.TabIndex = 4;
+            this.buttonRevert.Text = "revert";
+            this.buttonRevert.UseVisualStyleBackColor = true;
+            this.buttonRevert.Click += new System.EventHandler(this.ButtonRevert_Click);
+            // 
+            // buttonInverse
+            // 
+            this.buttonInverse.Location = new System.Drawing.Point(671, 58);
+            this.buttonInverse.Name = "buttonInverse";
+            this.buttonInverse.Size = new System.Drawing.Size(75, 23);
+            this.buttonInverse.TabIndex = 5;
+            this.buttonInverse.Text = "Inverse";
+            this.buttonInverse.UseVisualStyleBackColor = true;
+            this.buttonInverse.Click += new System.EventHandler(this.ButtonInverse_Click);
+            // 
+            // buttonBright
+            // 
+            this.buttonBright.Location = new System.Drawing.Point(671, 87);
+            this.buttonBright.Name = "buttonBright";
+            this.buttonBright.Size = new System.Drawing.Size(75, 23);
+            this.buttonBright.TabIndex = 6;
+            this.buttonBright.Text = "Brght Up";
+            this.buttonBright.UseVisualStyleBackColor = true;
+            this.buttonBright.Click += new System.EventHandler(this.ButtonBright_Click);
+            // 
+            // buttonBrght2
+            // 
+            this.buttonBrght2.Location = new System.Drawing.Point(671, 116);
+            this.buttonBrght2.Name = "buttonBrght2";
+            this.buttonBrght2.Size = new System.Drawing.Size(75, 23);
+            this.buttonBrght2.TabIndex = 7;
+            this.buttonBrght2.Text = "Brght Down";
+            this.buttonBrght2.UseVisualStyleBackColor = true;
+            this.buttonBrght2.Click += new System.EventHandler(this.ButtonBrght2_Click);
+            // 
+            // buttonGamma
+            // 
+            this.buttonGamma.Location = new System.Drawing.Point(671, 145);
+            this.buttonGamma.Name = "buttonGamma";
+            this.buttonGamma.Size = new System.Drawing.Size(75, 23);
+            this.buttonGamma.TabIndex = 8;
+            this.buttonGamma.Text = "Gamma Up";
+            this.buttonGamma.UseVisualStyleBackColor = true;
+            this.buttonGamma.Click += new System.EventHandler(this.ButtonGamma_Click);
+            // 
+            // buttonGammaDn
+            // 
+            this.buttonGammaDn.Location = new System.Drawing.Point(671, 174);
+            this.buttonGammaDn.Name = "buttonGammaDn";
+            this.buttonGammaDn.Size = new System.Drawing.Size(75, 23);
+            this.buttonGammaDn.TabIndex = 9;
+            this.buttonGammaDn.Text = "Gamma Dn";
+            this.buttonGammaDn.UseVisualStyleBackColor = true;
+            this.buttonGammaDn.Click += new System.EventHandler(this.ButtonGammaDn_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(671, 331);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 10;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.ButtonSave_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonGammaDn);
+            this.Controls.Add(this.buttonGamma);
+            this.Controls.Add(this.buttonBrght2);
+            this.Controls.Add(this.buttonBright);
+            this.Controls.Add(this.buttonInverse);
+            this.Controls.Add(this.buttonRevert);
+            this.Controls.Add(this.buttonGray);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "ImageFilterApp";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button buttonGray;
+        private System.Windows.Forms.Button buttonRevert;
+        private System.Windows.Forms.Button buttonInverse;
+        private System.Windows.Forms.Button buttonBright;
+        private System.Windows.Forms.Button buttonBrght2;
+        private System.Windows.Forms.Button buttonGamma;
+        private System.Windows.Forms.Button buttonGammaDn;
+        private System.Windows.Forms.Button buttonSave;
+    }
+}
+
